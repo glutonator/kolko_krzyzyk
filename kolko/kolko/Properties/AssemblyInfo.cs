01da56e0100495dee1e54de0feb7cb3b34a869bf
glutonator <glutonator@gmail.com>
2017-02-16 00:56:16 +0100

    nowy

--- kolko/kolko/Properties/AssemblyInfo.cs
+++ kolko/kolko/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("kolko")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("kolko")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d303a662-28ee-4066-aa72-c845ef9fa31f")]
+[assembly: Guid("aef247a9-7f45-45f6-8e1d-8438929e635a")]
 
 // Version information for an assembly consists of the following four values:
 //
