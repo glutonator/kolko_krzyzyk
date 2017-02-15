37b6ef35421ab9da847f88e66735989b523ebdf2
glutonator <glutonator@gmail.com>
2017-02-16 00:58:55 +0100

    poprawki początkowe

---
+++ kolko/kolko/Board.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Linq;
+
+namespace kolko
+{
+    public sealed class Board
+    {
+        private string board_string;
+        private int iterator;
+        //private ArrayList board_array;
+        //private Hashtable board_hashtbale;
+        private Dictionary<int,string> board_dictionary;
+
+        //singleton
+        private static readonly Board instace = new Board();
+        private Board()
+        {
+            init();
+        }
+        public static Board Instace
+        {
+            get
+            {
+                return instace;
+            }
+        }
+        //koniec deklaracji singletona
+
+        public void init ()
+        {
+            iterator = 0;
+            board_dictionary = new Dictionary<int, string>(9);
+            for (int i = 1; i < 10; i++)
+            {
+                string pom = i.ToString();
+                board_dictionary.Add(i, pom);
+            }
+        }
+
+        public string get_board_string ()
+        {
+            iterator++;
+            return board_string;
+        }
+        public int get_iterator()
+        {
+            return iterator;
+        }
+        public void set_board_hashtbale (int _iterator, string _charrr)
+        {
+            //board_dictionary.Wh
+            //int i = 5;
+
+            //tutaj coś jeszcze nie działa z tym wyszukiwaniem
+            //ogarnąć jak zrobic zmianę wratosci
+            //object ggg = board_dictionary.Where(i => { return i.Key == 2; } );
+            board_dictionary[_iterator] = _charrr;
+            //ggg.
+            //board_dictionary.where();
+            Console.Write("");
+
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(board_dictionary[7+i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(board_dictionary[4 + i]);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(board_dictionary[1 + i]);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ kolko/kolko/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace kolko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer _timer;
+            _timer = new Timer(3000);
+            _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+            _timer.Start();
+            //string board;
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //Board plansza = plansza.Instace();
+            //object board=Board.Instace.get_board_string();
+            //Console.Write(board);
+
+            //!Console.KeyAvailable
+            // while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+
+            }
+            Console.ReadKey(true);
+
+        }
+
+        private static void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.Clear();
+
+            object board = Board.Instace.get_board_string();
+            object buu = Board.Instace.get_iterator();
+            Board.Instace.set_board_hashtbale(2,"p");
+            //Console.Write(board);
+            //Console.Write(buu);
+            Board.Instace.print();
+
+
+            //throw new NotImplementedException();
+        }
+    }
+}
